3d47fa542b0fa9372442718437085752cd480df8
seifertj0 <seifertj@hs-pforzheim.de>
2024-06-19 11:44:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import TextLoader
 
 client = ElevenLabs(
-  api_key="sk_f716d692b18c5161aaef47ef205e88a413451699970727c1"
+  api_key="sk_348505f1ab3de49c73e9d72a4c591be44abf513a54e14796"
 )
 
 load_dotenv()
