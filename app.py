3c73010c0bd0dbc5466efa36fbced61fb2bafa95
Kessler1893 <kessler1893@web.de>
2024-06-19 12:05:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -153,20 +153,29 @@ if user_query is not None and user_query != "":
     st.session_state.chat_history.append(AIMessage(content=response))
 
 # conversation
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI"):
-          #Text 2 Speech
-          if TTS:
+if TTS:
+  for message in st.session_state.chat_history:
+      if isinstance(message, AIMessage):
+          with st.chat_message("AI"):
+            #Text 2 Speech
             voice_response = client.generate(
               text = message.content,
               voice = "PeterMeter",
               model = "eleven_multilingual_v2",
               output_format= "mp3_22050_32"
             )
-            save(voice_response, "response.mp3")
+            save(voice_response, "response.mp3") 
             autoplay_audio("response.mp3")
-          
+            
+            #Text 2 Text
+            st.write(message.content)
+      elif isinstance(message, HumanMessage):
+          with st.chat_message("Human"):
+              st.write(message.content)
+elif:
+  for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
           #Text 2 Text
           st.write(message.content)
     elif isinstance(message, HumanMessage):
